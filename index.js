87dbd8ff9e8ae1b37351fe2e7ed6d4efac07868e
TaylorDarneille <tmdarneille@dons.usfca.edu>
2020-10-30 15:00:14 -0700

    stubbed out auth routes

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ app.get('/', (req, res)=>{
     res.send("auth_practice home route")
 })
 
+app.get('/auth/signup', (req, res)=>{
+    res.send('GET signup.ejs')
+})
+
+app.post('/auth/signup', (req, res)=>{
+    res.send('POST form data from signup.ejs, then redirect')
+})
+
+app.get('/auth/login', (req, res)=>{
+    res.send('GET login.ejs')
+})
+
+app.post('/auth/login', (req, res)=>{
+    res.send('POST form data, validate users email/password')
+})
+
 app.listen(3000, ()=>{
     console.log("auth_practice running on port 3000")
 })
