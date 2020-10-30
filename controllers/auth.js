66d1a01d7e41460cf80727b8ba39875d1dbc2819
TaylorDarneille <tmdarneille@dons.usfca.edu>
2020-10-30 16:31:54 -0700

    changed signup post route to be a findOrCreate

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,15 +7,25 @@ router.get('/signup', (req, res)=>{
 })
 
 router.post('/signup', (req, res)=>{
-    db.user.create(req.body)
-    .then(createdUser=>{
-        console.log("just created the following user:", createdUser)
+    db.user.findOrCreate({
+        where: {email: req.body.email},
+        defaults: {
+            name: req.body.name,
+            password: req.body.password
+        }
+    })
+    .then(([createdUser, wasCreated])=>{
+        if(wasCreated){
+            console.log(`just created the following user:`, createdUser)
+            res.send('POST form data from signup.ejs, then redirect')
+        } else {
+            console.log('An account associated with that email address already exists! Did you mean to login?')
+            res.redirect('/auth/login')
+        }
     })
     .catch(err =>{
         console.log(err)
     })
-    console.log('signup form data:', req.body)
-    res.send('POST form data from signup.ejs, then redirect')
 })
 
 router.get('/login', (req, res)=>{
