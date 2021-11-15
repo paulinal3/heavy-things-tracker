a5c665d1d42ab3368f9d63b7675563ce95649b73
paulinal3 <paulinal3@outlook.com>
2021-11-14 19:28:18 -0500

    expanding limit on search

--- controllers/searchRoutes.js
+++ controllers/searchRoutes.js
@@ -11,9 +11,10 @@ router.get('/', (req, res) => {
 router.get('/results', (req, res) => {
     const exerciseApi = 'https://wger.de/api/v2/exercise/'
     const eng = 'language=2'
+    const limit = 'limit=500'
     let muscleGroup = req.query.category
 
-    axios.get(`${exerciseApi}?category=${muscleGroup}&${eng}`)
+    axios.get(`${exerciseApi}?category=${muscleGroup}&${eng}&${limit}`)
     .then(apiRes => {
         console.log('this is results of exercises\n', apiRes.data.results)
         const results = apiRes.data.results
