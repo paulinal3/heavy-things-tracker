1f9dfd16b3fb59554de84498752e678a5f1bf873
paulinal3 <paulinal3@outlook.com>
2021-11-14 19:17:11 -0500

    narrowed api results to data array based on search

--- controllers/searchRoutes.js
+++ controllers/searchRoutes.js
@@ -7,4 +7,17 @@ router.get('/', (req, res) => {
     res.render('search/index')
 })
 
+// create a search results route
+router.get('/results', (req, res) => {
+    const exerciseApi = 'https://wger.de/api/v2/exercise/'
+    const eng = 'language=2'
+    let muscleGroup = req.query.category
+
+    axios.get(`${exerciseApi}?category=${muscleGroup}&${eng}`)
+    .then(apiRes => {
+        console.log('this is results of exercises\n', apiRes.data.results)
+        res.render('search/results')
+    })
+})
+
 module.exports = router
