a95d017499ab464a9ff356f577e7e5cd9155f082
paulinal3 <paulinal3@outlook.com>
2021-11-16 11:19:12 -0500

    recreated search results page in exercise controller

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -13,4 +13,20 @@ router.get('/search', (req, res) => {
     res.render('exercises/search')
 })
 
+// create index route based on search results
+router.get('/search/results', (req, res) => {
+    const rootApi = 'https://v1.exercisedb.io/api/exercises'
+    let muscleTargeted = req.query.bodyPart
+
+    axios.get(`${rootApi}/bodyPart/${muscleTargeted}`, authHeader)
+    .then(apiRes => {
+        console.log('this is the apiRes data arr of exercises', apiRes.data)
+        const results = apiRes.data
+        res.render('exercises/index', {results})
+    })
+    .catch(error => {
+        console.error
+    })
+})
+
 module.exports = router
