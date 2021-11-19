cae810435492ce71da9d813f009a36eb9f595fdc
paulinal3 <paulinal3@outlook.com>
2021-11-19 14:27:03 -0500

    added exercises showing up in list but not changing name based on click

--- static/js/workouts.js
+++ static/js/workouts.js
@@ -1,9 +1,22 @@
+const addedArr = []
+let exerciseList = document.getElementById('exerciseList')
 const addSavedExercise = () => {
-    exerciseList.innerText = 'hello'
+    exerciseList.innerText = ''
+    let nameValue = document.getElementById('nameValue')
+    console.log('this is the name\n', nameValue.value)
+    addedArr.push(nameValue.value)
+    console.log('this is the addedArr\n', addedArr)
+    const olAdded = document.createElement('ol')
+    for (let i = 0; i < addedArr.length; i++) {
+        const liExercise = document.createElement('li')
+        liExercise.textContent = addedArr[i]
+        olAdded.appendChild(liExercise)
+    }
+    exerciseList.appendChild(olAdded)
 }
 
-document.querySelectorAll('.addBtn').forEach(addBtn => addBtn.addEventListener('click', addSavedExercise))
-
-// document.getElementsByClassName('addBtn').addEventListener('click', addSavedExercise, {
-//     once: true
-// })
+document.querySelectorAll('.addBtn').forEach(addBtn => {
+    addBtn.addEventListener('click', addSavedExercise, {
+        once: true
+    })
+})
